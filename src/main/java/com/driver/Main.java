3a750c40caa9dbaf0267082f7cfbb711a68b879b
silulitgithub <kumarsilu530@gmail.com>
2023-05-13 01:18:14 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //obj.name="name";
+
+        // Error: java: name has private access in com.driver.RWOnly
+
+        obj.setName("NAME");
+        System.out.println(obj.getName());
+    }
 }
